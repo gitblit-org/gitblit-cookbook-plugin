05f7eecc70cf6819e26a433459919b1195fbc6b0
James Moger <james.moger@gitblit.com>
2014-03-18 17:16:03 -0400

    Revise plugin example for dispatchers

--- src/main/java/com/gitblit/plugins/cookbook/GitblitCookbookPlugin.java
+++ src/main/java/com/gitblit/plugins/cookbook/GitblitCookbookPlugin.java
@@ -22,7 +22,11 @@ import ro.fortsoft.pf4j.Extension;
 import ro.fortsoft.pf4j.Plugin;
 import ro.fortsoft.pf4j.PluginWrapper;
 
-import com.gitblit.transport.ssh.CommandMetaData;
+import com.gitblit.manager.IGitblit;
+import com.gitblit.models.ServerStatus;
+import com.gitblit.models.UserModel;
+import com.gitblit.transport.ssh.commands.CommandMetaData;
+import com.gitblit.transport.ssh.commands.DispatchCommand;
 import com.gitblit.transport.ssh.commands.SshCommand;
 
 public class GitblitCookbookPlugin extends Plugin {
@@ -41,7 +45,25 @@ public class GitblitCookbookPlugin extends Plugin {
     // no op
   }
 
+  /**
+   * You must register a dispatcher for your custom commands.  The dispatcher
+   * must be annotated with CommandMetaData.  This annotation will assign the
+   * plugin command namespace and offer a description.
+   *
+   */
   @Extension
+  @CommandMetaData(name = "cookbook", description = "Example commands")
+  public static class CookbookDispatcher extends DispatchCommand {
+    @Override
+    protected void registerCommands(UserModel user) {
+        registerCommand(user, HelloWorldCommand.class);
+        registerCommand(user, StatusCommand.class);
+    }
+  }
+
+  /**
+   * Example HelloWorld command.
+   */
   @CommandMetaData(name = "hello", description = "Say hello in different languages")
   public static class HelloWorldCommand extends SshCommand {
 
@@ -55,6 +77,27 @@ public class GitblitCookbookPlugin extends Plugin {
     public void run() {
       final String greeting = (french ? "Bonjour " : "Hello ");
       stdout.print(greeting + name + "!\n");
+    }
+  }
+
+  /**
+   * Example status command.
+   */
+  @CommandMetaData(name = "status", description = "Display the server status")
+  public static class StatusCommand extends SshCommand {
+
+    @Override
+    public void run() {
+      IGitblit gitblit = getContext().getGitblit();
+      ServerStatus status = gitblit.getStatus();
+      stdout.print("version:        " + status.version + "\n");
+      stdout.print("releaseDate:    " + status.releaseDate + "\n");
+      stdout.print("isGO:           " + status.isGO + "\n");
+      stdout.print("container:      " + status.servletContainer + "\n");
+      stdout.print("bootDate:       " + status.bootDate + "\n");
+      stdout.print("heap maximum:   " + status.heapMaximum + "\n");
+      stdout.print("     allocated: " + status.heapAllocated + "\n");
+      stdout.print("     free:      " + status.heapFree + "\n");
     }
   }
 }
