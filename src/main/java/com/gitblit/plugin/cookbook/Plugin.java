76225d21fb91ad952001e1235e550597ebc65e0d
James Moger <james.moger@gitblit.com>
2014-04-17 18:03:00 -0400

    Update for plugin lifecycle methods

--- src/main/java/com/gitblit/plugin/cookbook/Plugin.java
+++ src/main/java/com/gitblit/plugin/cookbook/Plugin.java
@@ -17,6 +17,7 @@ package com.gitblit.plugin.cookbook;
 
 import ro.fortsoft.pf4j.Extension;
 import ro.fortsoft.pf4j.PluginWrapper;
+import ro.fortsoft.pf4j.Version;
 
 import com.gitblit.extensions.GitblitPlugin;
 import com.gitblit.models.UserModel;
@@ -25,33 +26,49 @@ import com.gitblit.transport.ssh.commands.DispatchCommand;
 
 public class Plugin extends GitblitPlugin {
 
-  public Plugin(PluginWrapper wrapper) {
-    super(wrapper);
-  }
+	public Plugin(PluginWrapper wrapper) {
+		super(wrapper);
+	}
 
-  @Override
-  public void start() {
-    // no op
-  }
+	@Override
+	public void start() {
+		log.info("{} STARTED.", getWrapper().getPluginId());
+	}
 
-  @Override
-  public void stop() {
-    // no op
-  }
+	@Override
+	public void stop() {
+		log.info("{} STOPPED.", getWrapper().getPluginId());
+	}
 
-  /**
-   * You must register a dispatcher for your custom commands.  The dispatcher
-   * must be annotated with CommandMetaData.  This annotation will assign the
-   * plugin command namespace and offer a description.
-   *
-   */
-  @Extension
-  @CommandMetaData(name = "cookbook", description = "Example commands")
-  public static class CookbookDispatcher extends DispatchCommand {
-    @Override
-    protected void setup(UserModel user) {
-        register(user, HelloWorldCommand.class);
-        register(user, StatusCommand.class);
-    }
-  }
+	@Override
+	public void onInstall() {
+		log.info("{} INSTALLED.", getWrapper().getPluginId());
+	}
+
+	@Override
+	public void onUpgrade(Version oldVersion) {
+		log.info("{} UPGRADED from {}.", getWrapper().getPluginId(), oldVersion);
+	}
+
+	@Override
+	public void onUninstall() {
+		log.info("{} UNINSTALLED.", getWrapper().getPluginId());
+	}
+
+
+	/**
+	 * You must register a dispatcher for your custom commands. The dispatcher
+	 * must be annotated with CommandMetaData. This annotation will assign the
+	 * plugin command namespace and offer a description.
+	 *
+	 */
+	@Extension
+	@CommandMetaData(name = "cookbook", description = "Example commands")
+	public static class CookbookDispatcher extends DispatchCommand {
+		@Override
+		protected void setup(UserModel user) {
+			register(user, HelloWorldCommand.class);
+			register(user, StatusCommand.class);
+		}
+	}
 }
